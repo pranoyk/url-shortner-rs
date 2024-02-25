c5d9bd8e44ecfd219fec1acbe520cb33e8c29327
Pranoy Kundu <pranoy1998k@gmail.com>
2024-02-25 21:45:03 +0530

    implement shorten url

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 mod urlstore;
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-async fn shorten(urlstore: web::Json<urlstore::ShortenRequest>) -> impl Responder {
-    HttpResponse::Ok().body(urlstore.url.clone())
+pub static URLSTORE: Lazy<Mutex<urlstore::UrlStore>> =
+    Lazy::new(|| Mutex::new(urlstore::UrlStore::new()));
+
+async fn shorten(url: web::Json<urlstore::ShortenRequest>) -> impl Responder {
+    let short_url = URLSTORE.lock().unwrap().shorten(&url.url);
+    HttpResponse::Ok().body(short_url)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(hello).service(
-            web::scope("/api")
-                .service(
-                    web::resource("/shorten")
-                        .route(web::post().to(shorten))
-                ),
+            web::scope("/api").service(web::resource("/shorten").route(web::post().to(shorten))),
         )
     })
     .bind(("127.0.0.1", 8080))?
--- src/urlstore/mod.rs
+++ src/urlstore/mod.rs
@@ -1,7 +1,31 @@
 use serde::Deserialize;
+use sha2::{Digest, Sha256};
+use std::collections::HashMap;
 
 #[derive(Deserialize)]
 pub struct ShortenRequest {
     pub url: String,
 }
 
+pub struct UrlStore {
+    urls: HashMap<String, String>,
+}
+
+impl UrlStore {
+    pub fn new() -> UrlStore {
+        UrlStore {
+            urls: HashMap::new(),
+        }
+    }
+
+    pub fn shorten(&mut self, url: &str) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(url.as_bytes());
+        let result = hasher.finalize();
+        let hash = format!("{:x}", result);
+        if !self.urls.contains_key(&hash) {
+            self.urls.insert(hash.clone(), url.to_owned());
+        }
+        format!("localhost:8080/{}", hash)
+    }
+}
