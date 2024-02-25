d0a2984f931258ca4dfe640e99651fa9fe1edf79
Pranoy Kundu <pranoy1998k@gmail.com>
2024-02-25 16:11:18 +0530

    create a post endpoint to shorten url

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod urlstore;
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+async fn shorten(urlstore: web::Json<urlstore::ShortenRequest>) -> impl Responder {
+    HttpResponse::Ok().body(urlstore.url.clone())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
+        App::new().service(hello).service(
+            web::scope("/api")
+                .service(
+                    web::resource("/shorten")
+                        .route(web::post().to(shorten))
+                ),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
